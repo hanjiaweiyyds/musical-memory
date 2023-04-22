77d3705d24740e6b5d1bca8b53762b677041148f
hanjiaweiyyds <3274155418@qq.com>
2023-04-22 21:09:15 +0800

    testHjw

---
+++ test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by mac on 2023/4/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        printf("hello world\n");
+        
+    }
+    return 0;
+}
